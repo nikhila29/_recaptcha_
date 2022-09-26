198680c208bed7e47adbb2041428d79e4a343b88
nikhila.badri <nikhila.badri@jupitice.com>
2022-09-26 17:32:49 +0530

    recaptcha

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    request = require('request');
+ 
+const app = express();
+   app.set('view engine', 'ejs');
+   app.set("views", path.join(__dirname, "views"));
+   app.use(express.static('public'));
+   app.use(bodyParser.urlencoded({extended: true}));
+   app.use(bodyParser.json());
+   var port = 3000;
+ 
+app.get('/', function (req, res) {
+    res.render('index');
+});
+ 
+app.post('/captcha', function(req, res) {
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
+  {
+    return res.json({"responseError" : "something goes to wrong"});
+  }
+  const secretKey = "6Lff8b0hAAAAAGz4feMF1pM0pVU5-YQczZ4xV20H";
+ 
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+ 
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+ 
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseError" : "Failed captcha verification"});
+    }
+    res.json({"responseSuccess" : "Sucess"});
+  });
+});
+ 
+app.listen(port, function(){
+    console.log('Server is running at port: ',port);
+});
